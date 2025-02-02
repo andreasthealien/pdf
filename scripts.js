466c5a7ec153a6a4ea53b872024760fbc9bd880f
Andreas Blum-Gjerding <97835206+andreasthealien@users.noreply.github.com>
2025-02-02 03:52:38 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,15 +1,34 @@
 // Array of PDF filenames (you can manually list them or use a backend to fetch them)
 const pdfFiles = [
-    "Regnerekkefølgen.pdf",
-    "Lineære funksjoner, stigningstall og konstantledd.pdf",
-    "Når bruke hvilken trignonomiske setning.pdf"
+    {
+        name: "Regnerekkefølgen",
+        file: "Regnerekkefølgen.pdf"
+    },
+    {
+        name: "Potens og kvadratrot (ikke ferdig)",
+        file: "Potens, kvadratrot og kvadrattall.pdf"
+    },
+    {
+        name: "Lineære funksjoner, stigningstall og konstantledd (ikke ferdig)",
+        file: "Lineære funksjoner, stigningstall og konstantledd.pdf"
+    },
+    {
+        name: "Avstanden mellom sola og jorda",
+        file: "PDF - Fysikk - Avstand mellom jorda og sola.pdf"
+    },
+    {
+        name: "Når bruke hvilken trignonomiske setning",
+        file: "Når bruke hvilken trignonomiske setning.pdf"
+    }
 ];
 
 // Get the <ul> element
 const pdfList = document.getElementById("pdf-list");
 
 // Loop through the PDF files and create <li> elements
-pdfFiles.forEach((file) => {
+pdfFiles.forEach((info) => {
+    const file = info.file;
+    const name = info.name;
     const listItem = document.createElement("li");
     const pdfPath = `pdfs/${file}`;
 
@@ -18,7 +37,7 @@ pdfFiles.forEach((file) => {
     viewLink.href = pdfPath;
     viewLink.target = "_blank";
     viewLink.className = "se-knapp";
-    viewLink.textContent = `Se: "${file}"`;
+    viewLink.textContent = `Se: "${name}"`;
 
     // Create "Download" link
     const downloadLink = document.createElement("a");
